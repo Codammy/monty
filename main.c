f483e3f3a93ecc810dd013a75636d2b4786c9bc1
Codam <damisco005@gmail.com>
2023-10-24 10:06:20 +0000

    new changes

--- main.c
+++ main.c
@@ -13,8 +13,8 @@
 int main(int ac, char **av)
 {
 	FILE *file;
-	size_t n = 0, line = 1, i;
-	char *buffer = NULL, *tmp, *instruction[2];
+	size_t n, line = 1, i;
+	char *buffer, *tmp, *instruction[2];
 
 	if (ac != 2)
 	{
@@ -29,6 +29,10 @@ int main(int ac, char **av)
 	}
 	while (1)
 	{
+		buffer = NULL;
+		n = 0;
+		instruction[0] = NULL;
+		instruction[1] = NULL;
 		if (getline(&buffer, &n, file) == -1)
 		{
 			if (feof(file) == 1)
@@ -36,7 +40,6 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't open file %s\n", av[1]);
 			exit(EXIT_FAILURE);
 		}
-		fflush(file);
 		if (strtok(buffer, "\n"))
 		{
 			tmp = strtok(buffer, " ");
@@ -47,12 +50,12 @@ int main(int ac, char **av)
 				tmp = strtok(NULL, " ");
 				i++;
 			}
-		printf("%s %s\n", instruction[0], instruction[1]);
-		free(instruction[0]);
-		free(instruction[1]);
 		}
+		i = 0;
+		printf("%s\n", instruction[i]);
+		free(buffer);
 	}
-	/*if (strcmp(instruction, "push") == 0)
+/*	if (strcmp(instruction, "push") == 0)
 		push(atoi(instruction[1]));
 	if (strcmp(instruction, "push") == 0)
 		pall();
